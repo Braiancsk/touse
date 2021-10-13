7fdbfb7b3d442982d07bb61cbf7ac30378080f6f
Braian Fellipe <74879806+Braiancsk@users.noreply.github.com>
2021-10-13 17:13:14 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,3 +24,24 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
+  //script para acionar o menu mobile
+  let burgerBox = document.querySelector('.hamburger-wrapper');
+  let burger = document.querySelector('.hamburger');
+  let menu = document.querySelector('.menu-mobile');
+  let bg = document.querySelector('.bg');
+
+
+  burgerBox.addEventListener('click', ()=>{
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('menu-active');
+    if(burger.classList.contains('is-active')){
+      document.body.style.overflow = 'hidden';
+      bg.style.display = 'block';
+    }else{
+      document.body.style.overflow = 'auto';
+      bg.style.display = 'none';
+    }
+  });
+
+  
