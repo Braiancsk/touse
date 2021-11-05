b67b7091007297a7c1a7d4ef551d0f8b73c9f4bb
Braian <braianfelipecsk123@hotmail.com>
2021-11-05 17:53:37 -0300

    Finalização de página e ajustes no nav de todas as paginas

--- js/anuncie.js
+++ js/anuncie.js
@@ -42,18 +42,90 @@ function handleSlide(val){
         firstSlide.classList.add('active-slide');
         secondSlide.classList.remove('active-slide');
         thirdSlide.classList.remove('active-slide');
-        console.log(firstSlide);
+    
     }else if(user[1].classList.contains('circle')){
         secondSlide.classList.add('active-slide');
         firstSlide.classList.remove('active-slide');
         thirdSlide.classList.remove('active-slide');
-        console.log(secondSlide);
+ 
     }else if(user[2].classList.contains('circle')){
         thirdSlide.classList.add('active-slide');
         firstSlide.classList.remove('active-slide');
         secondSlide.classList.remove('active-slide');
-        console.log(thirdSlide);
+
     }
 }
 
+//autoplay no slide
+/*
+function autoPlay(){
+    let firstSlide = slide[0];
+    let secondSlide = slide[1];
+    let thirdSlide = slide[2];
+    for(var i = 0; i < slide.length; i++);
+
+
+    firstSlide.classList.add('active-slide');
+    secondSlide.classList.remove('active-slide');
+    thirdSlide.classList.remove('active-slide');
+    user1.classList.add('circle');
+    user2.classList.remove('circle');
+    user3.classList.remove('circle');
+   
+}
+
+
+
+
+function autoPlay2(){
+    let firstSlide = slide[0];
+    let secondSlide = slide[1];
+    let thirdSlide = slide[2];
+    for(var i = 0; i < slide.length; i++);
+
+    secondSlide.classList.add('active-slide');
+    firstSlide.classList.remove('active-slide');
+    thirdSlide.classList.remove('active-slide');
+    user2.classList.add('circle');
+    user1.classList.remove('circle');
+    user3.classList.remove('circle');
+   
+}
+
+function autoPlay3(){
+    let firstSlide = slide[0];
+    let secondSlide = slide[1];
+    let thirdSlide = slide[2];
+    for(var i = 0; i < slide.length; i++);
+
+    thirdSlide.classList.add('active-slide');
+    firstSlide.classList.remove('active-slide');
+    secondSlide.classList.remove('active-slide');
+    user3.classList.add('circle');
+    user1.classList.remove('circle');
+    user2.classList.remove('circle');
+}
+
+
+const delay = setInterval(autoPlay, 9000);
+const delay2 = setInterval(autoPlay2, 3000);
+const delay3 = setInterval(autoPlay3, 6000);
+
+const clear = setInterval(clearDelay,9000);
+//const play = setInterval(playDelay,9000);
+function clearDelay(){
+    clearInterval(delay3);
+    clearInterval(delay2);
+    clearInterval(delay);
+}
+
+function playDelay(){
+const delay2 = setInterval(autoPlay2, 3000);
+const delay3 = setInterval(autoPlay3, 5000);
+const delay = setInterval(autoPlay, 7000);
+}
 handleSlide();
+*/
+
+
+
